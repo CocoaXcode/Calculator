0338c6080465e36656a9302f42e64c236fb94df9
CocoaXcode <zellsun@gmail.com>
2012-12-16 18:33:04 +0800

    Accomplished Assignment1: A Simple RPN Calculator

---
+++ Calculator/CalculatorBrain.h
@@ -0,0 +1,17 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by LoveBingbing on 12/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+- (void)pushOperand:(double)operand;
+- (double)performOperation:(NSString *)operation;
+- (void)clearStack;
+
+@end
